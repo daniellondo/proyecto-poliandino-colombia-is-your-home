8ef2c8ab49b5ed797e1f457bf41a8e2ea8a49882
Cristian <heavy.ca.ll@gmail.com>
2022-08-02 19:21:02 -0500

    Ventana modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,19 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+
+/* ventana emergete */
+const open2 = document.getElementById('open2');
+const modalcontainer2 = document.getElementById('modalcontainer2');
+const cerrarmodal2 = document.getElementById('cerrarmodal2');
+
+
+open2.addEventListener('click', () => {
+  modalcontainer2.classList.add('show');
+  alert ('prueba');
+});
+
+cerrarmodal2.addEventListener('click', () => {
+  modalcontainer2.classList.add('show');
+});
+
+  /* fin ventana emergente */
