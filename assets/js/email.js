60e7ef03d5b1280cbc96072833ab3e7d91858f73
Daniel Londoño <dlondono@arrivelogistics.com>
2022-08-08 15:16:42 -0500

    added new email js keys

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,11 +1,11 @@
-emailjs.init("aUyHGKH_UHuUqNE_D");
+emailjs.init("qIkuGeU_9NaOaoLH6");
 
 const btn = document.getElementById('button');
 
 document.getElementById('form').addEventListener('submit',function(event){
     event.preventDefault();
     btn.value = 'Enviando...';
-    const serviceID = 'service_s1y0soe';
+    const serviceID = 'service_o465mri';
     const templateID = 'template_us25cee';
 
     emailjs.sendForm(serviceID, templateID, this)
