10e01d34b49de85e9c1b32c532bb2730296d8cec
Daniel Londoño <dlondono@arrivelogistics.com>
2022-08-09 13:14:59 -0500

    added novedades page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,7 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
-
+(function()
+{ var i,e,d=document,s="script";i=d.createElement("script");
+i.async=1;i.charset="UTF-8"; i.src="https://cdn.curator.io/published/8944470b-db87-4eaa-a4b5-ca75c025c62b.js"; 
+e=d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e); 
+})();
