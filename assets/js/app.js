664e6e4624adf04bfd1135a001242c98ef8b234b
Daniel Londoño <dlondono@arrivelogistics.com>
2022-08-10 18:16:44 -0500

    testing video

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,8 @@ document.getElementById("year").innerHTML = new Date().getFullYear();
 i.async=1;i.charset="UTF-8"; i.src="https://cdn.curator.io/published/8944470b-db87-4eaa-a4b5-ca75c025c62b.js"; 
 e=d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e); 
 })();
+
+(function($){
+    $('#videoIndex').attr('controls');
+    $('#videoIndex').trigger('play');
+  })(jQuery);
